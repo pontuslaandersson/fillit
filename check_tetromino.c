08b9a476115c26a53b32cfb9bb635d339aad03ab
Pontus Andersson <panderss@c1r5p18.hive.fi>
2019-12-27 15:22:04 +0200

    Checker complete, maybe.

--- check_tetromino.c
+++ check_tetromino.c
@@ -6,36 +6,48 @@
 /*   By: panderss <panderss@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/20 13:22:15 by panderss          #+#    #+#             */
-/*   Updated: 2019/12/23 15:29:09 by panderss         ###   ########.fr       */
+/*   Updated: 2019/12/27 15:17:19 by panderss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "fillit.h"
 
-int		is_valid_shape(char *tetro)
+int		is_valid_shape(char *file)
 {
 	int		i;
 	int		j;
+	int		k;
+	int		c;
 
 	i = 0;
 	j = 0;
-	while (i <= 19)
+	while (file[c] != '\0')
 	{
-		if (tetro[i] == '#')
+		while (i <= 19)
 		{
-			if (i >= 5 && tetro[i - 5] == '#')
-				j++;
-			if (i <= 17 && tetro[i + 1] == '#')
-				j++;
-			if (i <= 13 && tetro[i + 5] == '#')
-				j++;
-			if (i >= 1 && tetro[i - 1] == '#')
-				j++;
+			if (file[i] == '#')
+			{
+				if (i >= 5 && file[i - 5] == '#')
+					j++;
+				if (i <= 17 && file[i + 1] == '#')
+					j++;
+				if (i <= 13 && file[i + 5] == '#')
+					j++;
+				if (i >= 1 && file[i - 1] == '#')
+					j++;
 		}
 		i++;
+		c++;
+		if (c % 21 == 0)
+		{
+			if (j != 6 && j != 8)
+				return (-1);
+			j = 0;
+			i = 0;
+		}
+		}
 	}
-	if (j == 6 || j == 8)
-		return (1);
 	return (0);
 }
 
@@ -55,47 +67,67 @@ int     check_file(char *file)
     if ((newline - 4) % 5 != 0 || (i - 20) % 21 != 0)
         return (-1);
     else
-        return (0);
+        return ((i - 20) / 21);
 }
 
-int		check_hash(char *tetro)
+int		check_hash(char *file)
 {
 	int i;
 	int	hash;
 	
 	i = 0;
 	hash = 0;
-	while (tetro[i] != '\0')
+	while (file[i] != '\0')
 	{
-		if (tetro[i] == '#')
+		if (file[i] == '#')
 			hash++;
 		++i;
 	}
-	return (hash);
+	if (hash % 4 != 0)
+	{
+		display_error();
+		return (-1);
+	}
+	return (0);
 }
 
-int		check_dot(char *tetro)
+int		check_dot(char *file)
 {
 	int i;
 	int	dot;
 	
 	i = 0;
 	dot = 0;
-	while (tetro[i] != '\0')
+	while (file[i] != '\0')
 	{
-		if (tetro[i] == '.')
+		if (file[i] == '.')
 			dot++;
 		++i;
-	}	
-	return (dot);
+	}
+	if (dot % 12 != 0)
+	{
+		display_error();
+		return (-1);
+	}
+	return (0);
 }
 
-int		check_newlines(char *tetro)
+int		check_newlines(char *file)
 {
-	if (tetro[4] != '\n' || tetro[9] != '\n' || tetro[14] != '\n' ||
-		   tetro[19] != '\n' || tetro[20] != '\n')
+	int i;
+	int	newline;
+	
+	i = 0;
+	newline = 0;
+	while (file[i] != '\0')
 	{
-		ft_putendl("Wrong number of newlines detected.");
+		if (file[i] == '\n')
+			newline++;
+		++i;
+	}
+	if ((newline - 4) % 5 != 0)
+	{
+		display_error();
 		return (-1);
 	}
 	return (0);
@@ -106,33 +138,19 @@ int		check_tetrominos(char *file)
 	int	hash;
 	int dot;
 	int	total;
+	int tetros;
 
-	if (check_file != 0)
+	if ((tetros = check_file(file)) <= 0)
 	{
 		display_error();
 		return (-1);
 	}
-	hash = check_hash(tetro);
-	dot = check_dot(tetro);
-	total = ft_strlen(tetro);
-	if (hash != 4 || dot != 12 || total != 22 
-		|| check_newlines(tetro) != 0 || is_valid_shape(tetro) == 0)
+	if (check_hash(file) != 0 || check_dot(file) != 0 || check_newlines(file) != 0 
+			|| is_valid_shape(file) != 0)
 	{
-		ft_putnbr(is_valid_shape(tetro));
-		ft_putendl("");
-		ft_putnbr(hash);
-		ft_putendl("");
-		ft_putnbr(dot);
-		ft_putendl("");
-		ft_putnbr(total);
-		ft_putendl("");
+		ft_putendl("Validation failed!");
+		display_error();
 		return (-1);
 	}
-	ft_putnbr(hash);
-	ft_putendl("");
-	ft_putnbr(dot);
-	ft_putendl("");
-	ft_putnbr(total);
-	ft_putendl("");
 	return (0);
 }
